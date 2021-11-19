458a535952000b2971baac0edc4636de6d29cf97
pinocchio22 <vviian.2@gmail.com>
2021-11-19 17:26:01 +0900

    feat : MainActivity.kt
    added fun setIpCamera , bindCameraUseCases

--- app/src/main/java/com/example/p2glet_lens/MainActivity.kt
+++ app/src/main/java/com/example/p2glet_lens/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.p2glet_lens
 import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
+import android.view.Surface
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.p2glet_lens.databinding.ActivityMainBinding
-import java.util.concurrent.Executor
+import java.lang.Exception
 import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors.newSingleThreadExecutor
+import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,13 +29,51 @@ class MainActivity : AppCompatActivity() {
     var imageCapture : ImageCapture? = null
     var imageAnalysis : ImageAnalysis? = null
 
-    var wudth = 320
+    var width = 320
     var height = 240
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        cameraExecutor = Executor.newSingleThreadExecutor()
+        cameraExecutor = Executors.newSingleThreadExecutor()
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+    }
+
+    fun setUpCamera() {
+        var cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+
+            cameraProvider = cameraProviderFuture.get()
+
+            bindCameraUseCases()
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    fun bindCameraUseCases() {
+        var rotation = Surface.ROTATION_270
+        var cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        preview = Preview.Builder()
+            .setTargetResolution(Size(width,height))
+            .setTargetRotation(rotation)
+            .build()
+        imageCapture = ImageCapture.Builder()
+            .setTargetResolution(Size(width,height))
+            .setTargetRotation(rotation)
+            .build()
+        imageAnalysis = ImageAnalysis.Builder() //color lens 구현할때 이용
+            .setTargetResolution(Size(width,height))
+            .setTargetRotation(rotation)
+            .build()
+
+        cameraProvider?.unbindAll()
+
+        try {
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis)
+            preview?.setSurfaceProvider(binding.viewFinder.createSurfaceProvider())
+        } catch (e : Exception) {
+            println(e)
+        }
     }
 }
